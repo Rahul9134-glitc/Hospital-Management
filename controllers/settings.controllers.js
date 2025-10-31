9c9afdc1197c9c9ae285d8823d6ae2ecc7f81ef6
Rahul vishwakarma <vishwakarmarahulramesh@gmail.com>
2025-10-31 09:47:30 +0530

    changing

--- controllers/settings.controllers.js
+++ controllers/settings.controllers.js
@@ -1,4 +1,4 @@
-import Settings from "../model/settings";
+import Settings from "../model/settings.js";
 const SETTINGS_ID = 'hospitalSettings';
 
 export const getSettings = async (req, res, next) => {
